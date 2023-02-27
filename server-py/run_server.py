45b5dd0fa71ef92482d33f2ce5854385f5dcdebc
Linus Horn <linushorn@gmx.net>
2023-02-27 08:03:49 +0100

    Update run_server.py

---
+++ server-py/run_server.py
@@ -0,0 +1,18 @@
+import waitress
+from main import app
+import requests
+import socket
+
+port = 80
+
+#get external ip
+R = requests.get("http://ip-api.com/json").json()
+IP = R["query"]
+print("Running on: " + IP, ":", port, "(external ip)")
+
+#get internal ip
+print("Running on: " + socket.gethostbyname(socket.gethostname()), ":", port, "(internal ip)")
+
+
+
+waitress.serve(app, listen="*" + ":" + str(port))
